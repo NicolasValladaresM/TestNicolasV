44261f92071f8aa5c3f59e0ce851486b48799fb7
Nicolas V <nicolas.valla20@gmail.com>
2024-10-31 17:06:22 -0300

    toInput fix

--- src/services/getMultiPrices.jsx
+++ src/services/getMultiPrices.jsx
@@ -9,42 +9,18 @@ const GetMultiPrices = () => {
     
     const user = getUserStorage().user;
 
-    // const headers = getUserStorage().headers;
     if (!user) {
       console.log("No se encontraron datos de usuario en localStorage");
       return;
     }
 
-      // const accessToken = headers.access_token;
-      // const uid = headers.uid;
-      // const client = headers.client;
-      // const expiry = headers.expiry;
-
-      // try {
-      //   const response = await fetch(
-      //     "https://api.qa.vitawallet.io/api/users/get_crypto_multi_prices",
-      //     {
-      //       method: "GET",
-      //       headers: {
-      //         "app-name": "ANGIE",
-      //         "access-token": accessToken,
-      //         uid: uid,
-      //         client: client,
-      //         expiry: expiry,
-      //       },
-      //     }
-      //   );
-      //   await handleApiResponse(response);
-      //   const data = await response.json();
-      //   return data.prices;
-      // } catch (error) {
-      //   console.log("Error al obtener valores de monedas", error);
-      // }
+     
 
 
     try {
       const response = await http.get("/users/get_crypto_multi_prices")
       await handleApiResponse(response);
+      console.log("Valores obtenidos getMultiprices", response);
       return response.data.prices;
     } catch (error) {
       console.log("Error al obtener valores de monedas", error);
